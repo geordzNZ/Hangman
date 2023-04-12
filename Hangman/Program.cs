5d7cdc8ef28cd5ec81848463733b1602ca9a50a5
GeordzNZ <geordie.king@gmail.com>
2023-04-12 23:02:25 +0100

    Populate and test wordList

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -10,6 +10,15 @@
             Console.WriteLine("\t\tHangman Word Guessing Game");
 
             Console.WriteLine("==========================================================\n");
+
+            // Word List
+            List<string> wordsList = new List<string>() { "Geordie", "Florian", "Michael", "Henrik" };
+
+            // Check List populated correctly
+            foreach (string word in wordsList) 
+            {
+                Console.WriteLine($"{word}");
+            }
         }
     }
 }
