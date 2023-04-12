237002d4b9f4d73446d1299a45a078c92468556a
GeordzNZ <geordie.king@gmail.com>
2023-04-12 22:49:10 +0100

    Add project files.

---
+++ Hangman/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Hangman
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Header section
+            Console.Clear();
+            Console.WriteLine("\t\t\tWELCOME");
+            Console.WriteLine("\t\tHangman Word Guessing Game");
+
+            Console.WriteLine("==========================================================\n");
+        }
+    }
+}
