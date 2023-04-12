d0b7ef79b558893077c1d548a22488e3d2350f6d
GeordzNZ <geordie.king@gmail.com>
2023-04-12 23:13:45 +0100

    Choose random word from list

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -15,10 +15,18 @@
             List<string> wordsList = new List<string>() { "Geordie", "Florian", "Michael", "Henrik" };
 
             // Check List populated correctly
+            Console.WriteLine($"Words in wordList: {wordsList.Count}");
             foreach (string word in wordsList) 
             {
-                Console.WriteLine($"{word}");
+                Console.WriteLine($" - {word}");
             }
+
+            // Get word from wordList - randomly
+            Random randomTargetID = new Random();
+            int targetWordID = randomTargetID.Next(wordsList.Count);
+            string targetWord = wordsList[targetWordID];
+
+            Console.WriteLine($"\nID: {targetWordID} -- {targetWord}");
         }
     }
 }
