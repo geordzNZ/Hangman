d0d33b72c6359458e8372ad89b618b52469d9bd2
GeordzNZ <geordie.king@gmail.com>
2023-04-13 00:20:14 +0100

    Check letter and populate / display output

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Hangman
+﻿using System.Collections.Generic;
+
+namespace Hangman
 {
     internal class Program
     {
@@ -21,12 +23,43 @@
                 Console.WriteLine($" - {word}");
             }
 
-            // Get word from wordList - randomly
+            // Get/store word from wordList - randomly
             Random randomTargetID = new Random();
-            int targetWordID = randomTargetID.Next(wordsList.Count);
-            string targetWord = wordsList[targetWordID];
+            int targetWordID = 0;
+            //int targetWordID = randomTargetID.Next(wordsList.Count);
+            string targetWord = wordsList[targetWordID].ToUpper();
+            List<char> outputList = new List<char>();
 
-            Console.WriteLine($"\nID: {targetWordID} -- {targetWord}");
+            Console.WriteLine($"\nID: {targetWordID} --> {targetWord}");
+
+            // Get user input
+            Console.Write($"\n\tPlease choose a letter: ");
+            string guessedLetterInput = Console.ReadLine().ToUpper();
+            char guessedLetter = char.Parse(guessedLetterInput);
+
+            Console.WriteLine($"Letter guessed is: {guessedLetter}");
+
+            // Check if letter is in word
+            //foreach (char letter in targetWord)
+            //{
+            //    Console.WriteLine($" - {letter} --> {letter == guessedLetter}");
+            //}
+            for (int i = 0; i < targetWord.Length; i++)
+            {
+                Console.WriteLine($" - {targetWord[i]} --> {targetWord[i] == guessedLetter}");
+                if (targetWord[i] == guessedLetter)
+                {
+                    outputList.Add(guessedLetter);
+                }
+                else
+                {
+                    outputList.Add('_');
+                }
+
+                // Output results
+                Console.WriteLine(String.Join(" ", outputList));
+            }
+
         }
     }
 }
