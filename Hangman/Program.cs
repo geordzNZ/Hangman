5617ac144020168363dbc7ab53af7d2e5fce80c0
GeordzNZ <geordie.king@gmail.com>
2023-04-13 02:37:26 +0100

    refactor user input and implement game loop

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -4,61 +4,87 @@ namespace Hangman
 {
     internal class Program
     {
+        // Word List
+        //const List<string> WORDSLIST = new List<string>() { "Geordie", "Florian", "Michael", "Henrik" };
+        const int MAX_GUESSES = 12;
         static void Main(string[] args)
         {
             // Header section
-            Console.Clear();
             Console.WriteLine("\t\t\tWELCOME");
             Console.WriteLine("\t\tHangman Word Guessing Game");
-
             Console.WriteLine("==========================================================\n");
 
             // Word List
             List<string> wordsList = new List<string>() { "Geordie", "Florian", "Michael", "Henrik" };
 
-            // Check List populated correctly
-            Console.WriteLine($"Words in wordList: {wordsList.Count}");
-            foreach (string word in wordsList) 
-            {
-                Console.WriteLine($" - {word}");
-            }
-
             // Get/store word from wordList - randomly
             Random randomTargetID = new Random();
-            int targetWordID = 0;
-            //int targetWordID = randomTargetID.Next(wordsList.Count);
+            int targetWordID = randomTargetID.Next(wordsList.Count);
             string targetWord = wordsList[targetWordID].ToUpper();
-            List<char> outputList = new List<char>();
 
-            Console.WriteLine($"\nID: {targetWordID} --> {targetWord}");
-
-            // Get user input
-            Console.Write($"\n\tPlease choose a letter: ");
-            string guessedLetterInput = Console.ReadLine().ToUpper();
-            char guessedLetter = char.Parse(guessedLetterInput);
-
-            Console.WriteLine($"Letter guessed is: {guessedLetter}");
-
-            // Check if letter is in word
-            //foreach (char letter in targetWord)
-            //{
-            //    Console.WriteLine($" - {letter} --> {letter == guessedLetter}");
-            //}
+            // Prefill output array
+            char[] outputChars = new char[targetWord.Length];
             for (int i = 0; i < targetWord.Length; i++)
             {
-                Console.WriteLine($" - {targetWord[i]} --> {targetWord[i] == guessedLetter}");
-                if (targetWord[i] == guessedLetter)
+                outputChars[i] = '_';
+            }
+            
+            //Game Loop
+            int wrongGuesses = 0;
+            int totalCorrectGuesses = 0;
+            do
+            {
+                // Get user input
+                Console.Write($"\n\tPlease choose a letter: ");
+                ConsoleKeyInfo guessedLetterInput = Console.ReadKey();
+                char guessedLetter = char.Parse(guessedLetterInput.Key.ToString());
+
+                int correctLetters = 0;
+                for (int i = 0; i < targetWord.Length; i++)
                 {
-                    outputList.Add(guessedLetter);
+                    if (targetWord[i] == guessedLetter)
+                    {
+                        outputChars[i] = guessedLetter;
+                        correctLetters++;
+                    }
+                }
+
+                if (correctLetters != 0)
+                {
+                    totalCorrectGuesses += correctLetters;
                 }
                 else
                 {
-                    outputList.Add('_');
+                    wrongGuesses++;
                 }
 
                 // Output results
-                Console.WriteLine(String.Join(" ", outputList));
-            }
+                Console.WriteLine($"\n\tYou guessed: {guessedLetter} ... {correctLetters} in the word.");
+
+                Thread.Sleep(3000);
+
+                Console.Clear();
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("\t\t\tWELCOME");
+                Console.WriteLine("\t\tHangman Word Guessing Game");
+                Console.WriteLine("==========================================================\n");
+                Console.WriteLine($"\t\t{String.Join(' ', outputChars)}");
+                
+                if (totalCorrectGuesses == targetWord.Length)
+                {
+                    Console.WriteLine($"\n\t\tYou guess correctly ... Well done!!\n");
+                    System.Environment.Exit(0);
+                }
+
+            } while (wrongGuesses < MAX_GUESSES);
+
+            Console.Clear();
+            Console.WriteLine("\t\t\tWELCOME");
+            Console.WriteLine("\t\tHangman Word Guessing Game");
+            Console.WriteLine("==========================================================\n");
+            Console.WriteLine($"\t\t{String.Join(' ',targetWord.ToCharArray())}");
+
+            Console.WriteLine($"\n\t\tYou didnt guess correctly ... Better luck next time!!\n");
 
         }
     }
