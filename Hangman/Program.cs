b789b7dc1b5e6f88667cc1bbfe34936021f1798d
GeordzNZ <geordie.king@gmail.com>
2023-04-14 01:58:24 +0100

    #1 Remove most of the duplicated heading codes

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -14,7 +14,7 @@ namespace Hangman
         //   |   |
         //   |  / \
         //__/|\__
-        const int MAX_GUESSES = 12;
+        const int MAX_GUESSES = 5;
         static void Main(string[] args)
         {
             // Word List
@@ -32,18 +32,18 @@ namespace Hangman
                 outputChars[i] = '_';
             }
 
-            // Header section
-            Console.WriteLine("\t\t\tWELCOME");
-            Console.WriteLine("\t\tHangman Word Guessing Game");
-            Console.WriteLine("==========================================================\n");
-            Console.WriteLine($"\t\t{String.Join(' ', outputChars)}");
-
-
             //Game Loop
             int wrongGuesses = 0;
             int totalCorrectGuesses = 0;
             do
             {
+                // Header section
+                Console.Clear();
+                Console.WriteLine("\t\t\tWELCOME");
+                Console.WriteLine("\t\tHangman Word Guessing Game");
+                Console.WriteLine("==========================================================\n");
+                Console.WriteLine($"\t\t{String.Join(' ', outputChars)}");
+
                 // Get user input
                 Console.Write($"\n\tPlease choose a letter: ");
                 ConsoleKeyInfo guessedLetterInput = Console.ReadKey();
@@ -69,33 +69,23 @@ namespace Hangman
                 }
 
                 // Output resultS
-                Console.WriteLine($"\n\tYou guessed: {guessedLetter} ... {correctLetters} in the word.");
+                Console.WriteLine($"\n\n\tYou guessed: {guessedLetter} ... {correctLetters} in the word.");
 
-                Thread.Sleep(3000);
-
-                Console.Clear();
-                Console.WriteLine("\t\t\tWELCOME");
-                Console.WriteLine("\t\tHangman Word Guessing Game");
-                Console.WriteLine("==========================================================\n");
-                Console.WriteLine($"\t\t{String.Join(' ', outputChars)}");
+                Thread.Sleep(2000);
                 
                 // Exit if word is found
                 if (totalCorrectGuesses == targetWord.Length)
                 {
-                    Console.WriteLine($"\n\t\tYou guess correctly ... Well done!!\n");
+                    Console.WriteLine($"\n\n\tYOU GUESSED '{String.Join(' ', targetWord.ToCharArray())}' CORRECTLY ... WELL DONE!!\n");
+                    Console.WriteLine("==========================================================\n");
                     return;
                 }
 
             } while (wrongGuesses < MAX_GUESSES);
 
             // Output if word not found
-            Console.Clear();
-            Console.WriteLine("\t\t\tWELCOME");
-            Console.WriteLine("\t\tHangman Word Guessing Game");
+            Console.WriteLine($"\n\n\tTHE WORD WAS '{String.Join(' ', targetWord.ToCharArray())}' ... BETTER LUCK NEXT TIME!!\n");
             Console.WriteLine("==========================================================\n");
-            Console.WriteLine($"\t\t{String.Join(' ',targetWord.ToCharArray())}");
-
-            Console.WriteLine($"\n\t\tYou didnt guess correctly ... Better luck next time!!\n");
 
         }
     }
